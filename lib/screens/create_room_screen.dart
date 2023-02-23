fd5b049dc0352ace07d80682eb3377f39dbfb91e
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-02-23 19:07:57 +1100

    create join and create room screen

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mp_tictactoe/responsive/responsive.dart';
+import 'package:mp_tictactoe/widgets/custom_button.dart';
+import 'package:mp_tictactoe/widgets/custom_text.dart';
+import 'package:mp_tictactoe/widgets/custom_textfield.dart';
 
 class CreateRoomScreen extends StatefulWidget {
   static const routeName = "/create-room-screen";
@@ -9,8 +13,50 @@ class CreateRoomScreen extends StatefulWidget {
 }
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  <Widget> [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.blue
+                  )
+                ],
+                text: 'Create Room',
+                fontSize: 70,
+              ),
+              SizedBox(height: size.height * 0.08,),
+              CustomTextField(
+                controller: _nameController,
+                hintText: 'Enter your nickname',
+
+              ),
+              SizedBox(height: size.height * 0.045,),
+              CustomButton(onTap: (){}, text: 'Create'),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mp_tictactoe/responsive/responsive.dart';
+import 'package:mp_tictactoe/widgets/custom_button.dart';
+import 'package:mp_tictactoe/widgets/custom_text.dart';
+import 'package:mp_tictactoe/widgets/custom_textfield.dart';
 
 class JoinRoomScreen extends StatefulWidget {
 
@@ -10,8 +14,59 @@ class JoinRoomScreen extends StatefulWidget {
 }
 
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _gameIdController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _nameController.dispose();
+    _gameIdController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  <Widget> [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                      blurRadius: 40,
+                      color: Colors.blue
+                  )
+                ],
+                text: 'Join Room',
+                fontSize: 70,
+              ),
+              SizedBox(height: size.height * 0.08,),
+              CustomTextField(
+                controller: _nameController,
+                hintText: 'Enter your nickname',
+
+              ),
+              const SizedBox(height: 20,),
+              CustomTextField(
+                controller: _nameController,
+                hintText: 'Enter game id',
+
+              ),
+              SizedBox(height: size.height * 0.045,),
+              CustomButton(onTap: (){}, text: 'Join'),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
